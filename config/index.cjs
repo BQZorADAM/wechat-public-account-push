e4741dfa01262b1bc505bce92b925ec997cd856f
BQZorADAM <115334981+BQZorADAM@users.noreply.github.com>
2022-10-08 21:50:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,50 +6,58 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-// 使用微信测试号：公众号APP_ID
+
+  // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx4251463dafa6e4b3',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '33c9475f57a103b2a36a8eee61c935a7',
 
- 
+  CITY: '咸阳市',
+
   USERS: [
     {
+      // 想要发送的人的名字
+      name: '欣欣',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLWaC6pkSXDaQ6IzpkIkv4YudDgg',
-     
+      id: 'oLWaC6owIes0c_HSlgWXTDYPYmeo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lsi51fzNDRZPBAiHKvRdG_na6b5JT05JqDd0KWj8QAI',
-     
-      // 和风天气apikey
-    weather_key: '1d7a287bad764c90a66f44186fb3018e',
-      
-      //所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
-    region: '咸阳市',
-// 生日1
- {
-          type: '生日', name: '傅欣同学', year: '2004', date: '02-23',
+      useTemplateId: 'rLvOHfrtMhzzfmNf5lBrGPF-ItHVlc2Ahc2AAma2MYA',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
         },
-// 生日2
- {
-          type: '生日', name: '白沁卓同学', year: '2004', date: '04-07',
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
-//在一起的日子，格式同上，暂不不支持农历
-    {keyword:'love_date': '2022-07-26'},
-//金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
-note_ch: '',
-//金句英文
-note_en: ''  
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
     },
   ],
-}   
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLWaC6sWZC5ofo2ZfPOGVEs9ahmA',
+      id: '',
+    }
+  ],
+
 }
 
 module.exports = USER_CONFIG
-
